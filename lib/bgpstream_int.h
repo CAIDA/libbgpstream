e8279b4d374c68f36ac2abc2d2fc653f430ce4b6
Chiara Orsini <chiara@caida.org>
2014-08-11 21:22:56 +0000

    And bgpstream_int (to hide internal structures)

---
+++ lib/bgpstream_int.h
@@ -0,0 +1,49 @@
+/*
+ * libbgpstream
+ *
+ * Chiara Orsini, CAIDA, UC San Diego
+ * chiara@caida.org
+ *
+ * Copyright (C) 2013 The Regents of the University of California.
+ *
+ * This file is part of libbgpstream.
+ *
+ * libbgpstream is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libbgpstream is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with libbgpstream.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _BGPSTREAM_INT_H
+#define _BGPSTREAM_INT_H
+
+#include "bgpstream_datasource.h"
+#include "bgpstream_input.h"
+#include "bgpstream_reader.h"
+#include "bgpstream_filter.h"
+#include "bgpstream_record.h"
+#include "bgpstream_elem.h"
+
+
+typedef enum {ALLOCATED, ON, OFF} bgpstream_status;
+
+typedef struct struct_bgpstream_t {  
+  bgpstream_input_mgr_t *input_mgr;
+  bgpstream_reader_mgr_t *reader_mgr;
+  bgpstream_filter_mgr_t *filter_mgr;
+  bgpstream_datasource_mgr_t *datasource_mgr;
+  bgpstream_status status;  
+} bgpstream_t;
+
+
+
+#endif /* _BGPSTREAM_INT_H */
