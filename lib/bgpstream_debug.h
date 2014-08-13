ad0d1b6192c65f34478f91af63eee7248dabeb05
Chiara Orsini <chiara@caida.org>
2014-08-13 18:57:59 +0000

    minor edits: bgpstream_debug

--- lib/bgpstream_debug.h
+++ lib/bgpstream_debug.h
@@ -39,16 +39,16 @@
 #define bgpstream_log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 
 // check will make sure the condition A is true, and if not logs the error M (with variable arguments for log_err), then jumps to the function's error: for cleanup.
-#define bgpstream_check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+#define bgpstream_check(A, M, ...) if(!(A)) { bgpstream_log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
 // sentinel is placed in any part of a function that shouldn't run, and if it does prints an error message then jumps to the error: label. You put this in if-statements and switch-statements to catch conditions that shouldn't happen, like the default:.
-#define bgpstream_sentinel(M, ...)  { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+#define bgpstream_sentinel(M, ...)  { bgpstream_log_err(M, ##__VA_ARGS__); errno=0; goto error; }
 
 // check_mem that makes sure a pointer is valid, and if it isn't reports it as an error with "Out of memory."
-#define bgpstream_check_mem(A) check((A), "Out of memory.")
+#define bgpstream_check_mem(A) bgpstream_check((A), "Out of memory.")
 
 // An alternative macro check_debug that still checks and handles an error, but if the error is common then you don't want to bother reporting it. In this one it will use debug instead of log_err to report the message, so when you define NDEBUG the check still happens, the error jump goes off, but the message isn't printed.
-#define bgpstream_check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; }
+#define bgpstream_check_debug(A, M, ...) if(!(A)) { bgpstream_debug(M, ##__VA_ARGS__); errno=0; goto error; }
 
 
 #endif /* _BGPSTREAM_DEBUG_H */
