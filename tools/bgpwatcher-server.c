95fae23ca93739b4f5575848b5beeea3943ba29e
Alistair King <alistair@caida.org>
2014-09-18 09:35:24 -0700

    add tool to drive bgpwatcher

---
+++ tools/bgpwatcher-server.c
@@ -0,0 +1,178 @@
+/*
+ * bgpwatcher
+ *
+ * Alistair King, CAIDA, UC San Diego
+ * corsaro-info@caida.org
+ *
+ * Copyright (C) 2012 The Regents of the University of California.
+ *
+ * This file is part of bgpwatcher.
+ *
+ * bgpwatcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bgpwatcher is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bgpwatcher.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "config.h"
+
+#include <assert.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/* include bgpwatcher's public interface */
+/* @@ never include the _int.h file from tools. */
+#include "bgpwatcher.h"
+
+/** Indicates that bgpwatcher is waiting to shutdown */
+volatile sig_atomic_t bgpwatcher_shutdown = 0;
+
+/** The number of SIGINTs to catch before aborting */
+#define HARD_SHUTDOWN 3
+
+static bgpwatcher_t *watcher = NULL;
+
+/** Handles SIGINT gracefully and shuts down */
+static void catch_sigint(int sig)
+{
+  bgpwatcher_shutdown++;
+  if(bgpwatcher_shutdown == HARD_SHUTDOWN)
+    {
+      fprintf(stderr, "caught %d SIGINT's. shutting down NOW\n",
+	      HARD_SHUTDOWN);
+      exit(-1);
+    }
+
+  fprintf(stderr, "caught SIGINT, shutting down at the next opportunity\n");
+
+  if(watcher != NULL)
+    {
+      bgpwatcher_stop(watcher);
+    }
+
+  signal(sig, catch_sigint);
+}
+
+static void usage(const char *name)
+{
+  fprintf(stderr,
+	  "usage: %s [<options>]\n"
+	  "       -c <client-uri>    0MQ-style URI to listen for clients on\n"
+	  "                          (default: %s)\n"
+	  "       -i <interval-ms>   Time in ms between heartbeats to clients\n"
+	  "                          (default: %d)\n"
+	  "       -l <beats>         Number of heartbeats that can go by before \n"
+	  "                          a server is declared dead (default: %d)\n",
+	  name,
+	  BGPWATCHER_CLIENT_URI_DEFAULT,
+	  BGPWATCHER_HEARTBEAT_INTERVAL_DEFAULT,
+	  BGPWATCHER_HEARTBEAT_LIVENESS_DEFAULT);
+}
+
+int main(int argc, char **argv)
+{
+  /* for option parsing */
+  int opt;
+  int prevoptind;
+
+  /* to store command line argument values */
+  const char *client_uri = NULL;
+
+  uint64_t heartbeat_interval = BGPWATCHER_HEARTBEAT_INTERVAL_DEFAULT;
+  int heartbeat_liveness      = BGPWATCHER_HEARTBEAT_LIVENESS_DEFAULT;
+
+  signal(SIGINT, catch_sigint);
+
+  while(prevoptind = optind,
+	(opt = getopt(argc, argv, ":c:i:l:v?")) >= 0)
+    {
+      if (optind == prevoptind + 2 && *optarg == '-' ) {
+        opt = ':';
+        -- optind;
+      }
+      switch(opt)
+	{
+	case ':':
+	  fprintf(stderr, "ERROR: Missing option argument for -%c\n", optopt);
+	  usage(argv[0]);
+	  return -1;
+	  break;
+
+	case 'c':
+	  client_uri = optarg;
+	  break;
+
+	case 'i':
+	  heartbeat_interval = atoi(optarg);
+	  break;
+
+	case 'l':
+	  heartbeat_liveness = atoi(optarg);
+	  break;
+
+	case '?':
+	case 'v':
+	  fprintf(stderr, "bgpwatcher version %d.%d.%d\n",
+		  BGPWATCHER_MAJOR_VERSION,
+		  BGPWATCHER_MID_VERSION,
+		  BGPWATCHER_MINOR_VERSION);
+	  usage(argv[0]);
+	  return 0;
+	  break;
+
+	default:
+	  usage(argv[0]);
+	  return -1;
+	  break;
+	}
+    }
+
+  /* NB: once getopt completes, optind points to the first non-option
+     argument */
+
+  if((watcher = bgpwatcher_init()) == NULL)
+    {
+      fprintf(stderr, "ERROR: could not initialize bgpwatcher server\n");
+      goto err;
+    }
+
+  if(client_uri != NULL)
+    {
+      bgpwatcher_set_client_uri(watcher, client_uri);
+    }
+
+  bgpwatcher_set_heartbeat_interval(watcher, heartbeat_interval);
+
+  bgpwatcher_set_heartbeat_liveness(watcher, heartbeat_liveness);
+
+  /* do work */
+  /* this function will block until the server shuts down */
+  bgpwatcher_start(watcher);
+
+  /* this will always be set, normally to a SIGINT-caught message */
+  bgpwatcher_perr(watcher);
+
+  /* cleanup */
+  bgpwatcher_free(watcher);
+
+  /* complete successfully */
+  return 0;
+
+ err:
+  if(watcher != NULL) {
+    bgpwatcher_free(watcher);
+  }
+  return -1;
+}
