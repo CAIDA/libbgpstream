fc47b04f799fbb99e121e26b86da23a9f3d1c06e
Chiara Orsini <chiara@caida.org>
2014-11-03 10:45:07 -0800

    Added a new dispatcher function to dump AS visibility over time

--- lib/libbgpstore/bgpstore_interests_dispatcher.c
+++ lib/libbgpstore/bgpstore_interests_dispatcher.c
@@ -34,29 +34,60 @@
 #include "bl_bgp_utils.h"
 #include "bl_str_set.h"
 #include "bl_id_set.h"
+#include "bl_pfx_set.h"
 
 #include "khash.h"
 #include <assert.h>
 
 
+#define METRIC_PREFIX "bgp.visibility"
+
+
+
 /** Interests structures and functions */
 
+
+/* ############################################ */
+/*           Status of a bgpview                */
+/* ############################################ */
 
 typedef struct struct_bgpviewstatus_interest {
   // TODO
 } bgpviewstatus_interest_t;
 
-static bgpviewstatus_interest_t* bgpviewstatus_interest_create(bgpview_t *bgp_view, uint32_t ts)
+static bgpviewstatus_interest_t* bgpviewstatus_interest_create(bgpview_t *bgp_view,
+							       uint32_t ts)
 {
   bgpviewstatus_interest_t *bvstatus = (bgpviewstatus_interest_t *)malloc_zero(sizeof(bgpviewstatus_interest_t));
-  return bvstatus;
-}
 
-static int bgpviewstatus_interest_send(char *client_name, bgpviewstatus_interest_t* bvstatus)
-{
-  // TODO
-  fprintf(stderr, "Sending BGPVIEWSTATUS to client: %s\n", client_name);
-  return 0;
+  // --------------------------------------------------------------------
+  khiter_t k;
+  uint16_t i;
+  active_peer_status_t *aps;
+  printf("TIME: %d\n", ts);
+  printf("\tC: %d \tA: %d \tI: %d \n",
+	 kh_size(bgp_view->done_clients),
+	 kh_size(bgp_view->active_peers_info),
+	 kh_size(bgp_view->inactive_peers));
+  printf("ipv4: %d \tipv6: %d\n",
+	 kh_size(bgp_view->aggregated_pfxview_ipv4),
+	 kh_size(bgp_view->aggregated_pfxview_ipv6));
+  for (k = kh_begin(bgp_view->active_peers_info);
+       k != kh_end(bgp_view->active_peers_info); ++k)
+    {
+      if (kh_exist(bgp_view->active_peers_info, k))	
+	{
+	  i = kh_key(bgp_view->active_peers_info, k);
+	  aps = &(kh_value(bgp_view->active_peers_info, k));
+	  printf("%d: tables (%d - %d)\n\tipv4: %d \tipv6: %d\n",
+		 i, aps->expected_pfx_tables_cnt, aps->received_pfx_tables_cnt,
+		 aps->recived_ipv4_pfx_cnt, aps->recived_ipv6_pfx_cnt);
+	  printf("\n");
+	}
+    }
+  // --------------------------------------------------------------------
+	     
+  return bvstatus;
 }
 
 static void bgpviewstatus_interest_destroy(bgpviewstatus_interest_t* bvstatus)
@@ -68,11 +99,198 @@ static void bgpviewstatus_interest_destroy(bgpviewstatus_interest_t* bvstatus)
 }
 
 
+/* ############################################ */
+/*           Per AS aggregation                 */
+/* ############################################ */
+
+
+// AS -> prefix set
+
+KHASH_INIT(as_visibility /* name */, 
+	   uint32_t /* khkey_t */, 
+	   bl_ipv4_pfx_set_t* /* khval_t */, 
+	   1  /* kh_is_set */,
+	   kh_int_hash_func /*__hash_func */,  
+	   kh_int_hash_equal /* __hash_equal */);
+
+typedef khash_t(as_visibility) as_visibility_t;
+
+
+typedef struct struct_perasvisibility_interest {
+  /** Set of peers that are complete (i.e. no more
+   *  pfx tables expected) and satisfy the full feed
+   *  requirements (i.e. #ipv4 prefixes > 500K, 
+   *  #ipv6 prefixes > 10K ~ TODO check! )
+   */
+  bl_id_set_t *eligible_peers;
+  /** for each AS we store the unique set of prefixes
+   *  that are originated by this AS */
+  as_visibility_t *as_vis_map;
+    // TODO: add other structures??
+} perasvisibility_interest_t;
+
+
+
+static void as_visibility_insert(as_visibility_t *as_vis_map, uint32_t asn, bl_ipv4_pfx_t *pfx)
+{
+  int khret;
+  khiter_t k;
+  bl_ipv4_pfx_set_t* pfx_set;
+  if((k = kh_get(as_visibility, as_vis_map, asn)) == kh_end(as_vis_map))
+    {
+      k = kh_put(as_visibility, as_vis_map, asn, &khret);
+      kh_value(as_vis_map,k) = bl_ipv4_pfx_set_create();
+    }
+  pfx_set = kh_value(as_vis_map,k);
+  if(pfx_set == NULL){
+    // TODO: error
+  }
+  bl_ipv4_pfx_set_insert(pfx_set, *pfx);
+}
+
+
+
+static void perasvisibility_interest_destroy(perasvisibility_interest_t* peras_vis)
+{
+  if(peras_vis !=NULL)
+    {
+      if(peras_vis->eligible_peers != NULL)
+	{
+	  bl_id_set_destroy(peras_vis->eligible_peers);
+	  peras_vis->eligible_peers = NULL;
+	}
+      if(peras_vis->as_vis_map != NULL)
+	{
+	  kh_free_vals(as_visibility, peras_vis->as_vis_map, bl_ipv4_pfx_set_destroy);
+	  kh_destroy(as_visibility, peras_vis->as_vis_map);
+	  peras_vis->as_vis_map = NULL;
+	}      
+      free(peras_vis);
+    }
+}
+
+
+static perasvisibility_interest_t* perasvisibility_interest_create(bgpview_t *bgp_view,
+								   uint32_t ts)
+{
+  perasvisibility_interest_t *peras_vis = (perasvisibility_interest_t *) malloc_zero(sizeof(perasvisibility_interest_t));
+
+  // create internal structures
+  if((peras_vis->eligible_peers = bl_id_set_create()) == NULL)
+    {
+      fprintf(stderr, "Error: unable to create the eligible_peers set\n");
+      goto err;
+    }
+  
+  if((peras_vis->as_vis_map = kh_init(as_visibility)) == NULL)
+    {
+      fprintf(stderr, "Error: unable to create the eligible_peers set\n");
+      goto err;
+    }
+
+  
+  khiter_t k;
+  uint16_t i;
+  active_peer_status_t *aps;
+  
+  // 1. we select full feed peers that are complete (i.e. no more pfx table expected)
+  for (k = kh_begin(bgp_view->active_peers_info);
+       k != kh_end(bgp_view->active_peers_info); ++k)
+    {
+      if (kh_exist(bgp_view->active_peers_info, k))	
+	{
+	  i = kh_key(bgp_view->active_peers_info, k);
+	  aps = &(kh_value(bgp_view->active_peers_info, k));
+	  // check completeness
+	  if(aps->expected_pfx_tables_cnt == aps->received_pfx_tables_cnt)
+	    {
+	      // check full feed
+	      if(aps->recived_ipv4_pfx_cnt > 500000 ||
+		 aps->recived_ipv6_pfx_cnt > 10000 )
+		{
+		  bl_id_set_insert(peras_vis->eligible_peers, i);
+		}
+	    }	  
+	}
+    }
+  
+  // 2. go through all ipv4 prefixes, get their origin ASes
+  //    and populate the as visibility map
+  
+  bl_ipv4_pfx_t *pfx;
+  peerview_t *pv;
+  pfxinfo_t *pi;
+  khiter_t k_inn;
+
+  for (k = kh_begin(bgp_view->aggregated_pfxview_ipv4);
+       k != kh_end(bgp_view->aggregated_pfxview_ipv4); ++k)
+    {
+      if (kh_exist(bgp_view->aggregated_pfxview_ipv4, k))	
+	{
+	  pfx = &(kh_key(bgp_view->aggregated_pfxview_ipv4,k));
+	  pv = kh_value(bgp_view->aggregated_pfxview_ipv4,k);
+	  // for each id check if it is eligible
+	  for (k_inn = kh_begin(pv); k_inn != kh_end(pv); ++k_inn)
+	    {
+	      if (kh_exist(pv, k_inn))	
+		{
+		  i = kh_key(pv, k_inn);
+		  if(bl_id_set_exists(peras_vis->eligible_peers,i) == 1)
+		    {
+		      pi = &(kh_value(pv,k_inn));
+		      // get origin AS
+		      if(pi->orig_asn != 0) // not a special case
+			{
+			  // insert (AS, ipv4_pfx)
+			  as_visibility_insert(peras_vis->as_vis_map, pi->orig_asn, pfx);
+			}
+		    }
+		}
+	    }
+	}
+    }
+
+
+  // 3. print the results
+  uint32_t asn;
+  uint64_t ipv4_pfx_cnt;
+  for (k = kh_begin(peras_vis->as_vis_map);
+       k != kh_end(peras_vis->as_vis_map); ++k)
+    {
+      if (kh_exist(peras_vis->as_vis_map, k))	
+	{
+	  asn = kh_key(peras_vis->as_vis_map,k);
+	  ipv4_pfx_cnt = kh_size(kh_value(peras_vis->as_vis_map,k));
+	  // 
+	  fprintf(stdout,
+		  METRIC_PREFIX".ipv4.%"PRIu32" %"PRIu64" %"PRIu32"\n",
+		  asn,
+		  ipv4_pfx_cnt,
+		  ts);
+	}
+
+    }
+  
+  return peras_vis;
+
+ err:
+  perasvisibility_interest_destroy(peras_vis);
+  return NULL;
+}
+
+
+
+
+
+
+/** Dispatcher structures and functions */
+
 
 typedef struct struct_bgpstore_interests_dispatcher {
 
   // TODO: a structure for every possible interest
   bgpviewstatus_interest_t *bvstatus;
+  perasvisibility_interest_t *peras_vis;
 
 } bgpstore_interests_dispatcher_t;
 
@@ -84,6 +302,13 @@ static bgpstore_interests_dispatcher_t *bgpstore_interests_dispatcher_create()
   // interests are created on demand
   bid->bvstatus = NULL;
   return bid;
+}
+
+
+static int bgpstore_interests_dispatcher_send(bgpstore_interests_dispatcher_t *bid, char *client)
+{
+  // TODO: send interests related to client
+  return 0;
 }
 
 
@@ -135,9 +360,28 @@ int bgpstore_interests_dispatcher_run(clientinfo_map_t *active_clients, bgpview_
 		      fprintf(stderr, "ERROR: could not create bgpstore bgpviewstatus interest\n");
 		      return -1;
 		    }
-		}	      
-	      bgpviewstatus_interest_send(client_name, bid->bvstatus);
+		}
 	    }
+
+
+	    // if(cl_status->consumer_interests & BGPWATCHER_CONSUMER_INTEREST_ASVISIBILITY)
+	    {
+	      if(bid->peras_vis == NULL)
+		{
+		  bid->peras_vis = perasvisibility_interest_create(bgp_view, ts);
+		  if(bid->peras_vis == NULL)
+		    {
+		      fprintf(stderr, "ERROR: could not create bgpstore AS visibility interest\n");
+		      return -1;
+		    }
+		}
+	    }
+
+	    
+	  // TODO: satisfy other interests
+
+	  bgpstore_interests_dispatcher_send(bid, client_name);
+
 	}
     }
 
