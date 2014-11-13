0577295ee4298af2f444e87a364e0a7e40045b0b
Chiara Orsini <chiara@caida.org>
2014-11-12 18:53:24 -0800

    Worked on filtervis implementation

--- lib/plugins/bgpcorsaro_filtervis.c
+++ lib/plugins/bgpcorsaro_filtervis.c
@@ -45,6 +45,11 @@
 #include "khash.h"
 #include "bgpcorsaro_filtervis.h"
 
+#include "bl_bgp_utils.h"
+#include "bl_peersign_map.h"
+#include "bl_pfx_set.h"
+#include "bl_id_set.h"
+
 
 
 /** @file
@@ -77,9 +82,373 @@ static bgpcorsaro_plugin_t bgpcorsaro_filtervis_plugin = {
 
 
 
+/** Filter visibility core functions and structures */
+
+KHASH_INIT(peer_ipv4prefix_map /* name */, 
+	   uint16_t /* khkey_t */, 
+	   bl_ipv4_pfx_set_t * /* khval_t */, 
+	   1  /* kh_is_map */, 
+	   kh_int_hash_func /*__hash_func */,  
+	   kh_int_hash_equal /* __hash_equal */);
+
+typedef khash_t(peer_ipv4prefix_map) peer_ipv4prefix_map_t;
+
+KHASH_INIT(peer_ipv6prefix_map /* name */, 
+	   uint16_t /* khkey_t */, 
+	   bl_ipv6_pfx_set_t * /* khval_t */, 
+	   1  /* kh_is_map */, 
+	   kh_int_hash_func /*__hash_func */,  
+	   kh_int_hash_equal /* __hash_equal */);
+
+typedef khash_t(peer_ipv6prefix_map) peer_ipv6prefix_map_t;
+
+typedef struct struct_peer_breakdown_t {
+  uint16_t full_feed_peers_cnt;
+  uint16_t all_peers_cnt;
+} peer_breakdown_t;
+
+
+KHASH_INIT(ipv4prefix_peer_map /* name */, 
+	   bl_ipv4_pfx_t /* khkey_t */, 
+	   peer_breakdown_t /* khval_t */, 
+	   1  /* kh_is_map */, 
+	   bl_ipv4_pfx_hash_func /*__hash_func */,  
+	   bl_ipv4_pfx_hash_equal /* __hash_equal */);
+
+typedef khash_t(ipv4prefix_peer_map) ipv4prefix_peer_map_t;
+
+KHASH_INIT(ipv6prefix_peer_map /* name */, 
+	   bl_ipv6_pfx_t /* khkey_t */, 
+	   peer_breakdown_t /* khval_t */, 
+	   1  /* kh_is_map */, 
+	   bl_ipv6_pfx_hash_func /*__hash_func */,  
+	   bl_ipv6_pfx_hash_equal /* __hash_equal */);
+
+typedef khash_t(ipv6prefix_peer_map) ipv6prefix_peer_map_t;
+
+
+
 typedef struct struct_filter_vis_t {
-  int filter;
+  int start_time;
+  int end_time;
+  bl_peersign_map_t *ps_map;
+  peer_ipv4prefix_map_t *ipv4_vis;
+  peer_ipv6prefix_map_t *ipv6_vis;
+  // OTHER ?
+  int min_ipv4_mask_len;
+  int max_ipv4_mask_len;
+  int max_ipv6_mask_len ;
+  int ipv4_full_feed_th;
+  int ipv6_full_feed_th;
 } filter_vis_t;
+
+static filter_vis_t *filter_vis_create(){
+  // TODO
+  filter_vis_t *fv = NULL;
+  fv = (filter_vis_t*)malloc_zero(sizeof(filter_vis_t));
+  if(fv != NULL)
+    {
+      fv->start_time = 0;
+      fv->end_time = 0;
+      fv->ps_map = bl_peersign_map_create();
+      assert(fv->ps_map != NULL);
+      fv->ipv4_vis = kh_init(peer_ipv4prefix_map);
+      assert(fv->ipv4_vis != NULL);
+      fv->ipv6_vis = kh_init(peer_ipv6prefix_map);
+      assert(fv->ipv6_vis != NULL);
+      fv->min_ipv4_mask_len = 7;
+      fv->max_ipv4_mask_len = 7;
+      fv->max_ipv6_mask_len = 64;
+      fv->ipv4_full_feed_th = 400000;
+      fv->ipv6_full_feed_th = 10000;
+    }
+  return fv;
+}
+
+
+static void filter_vis_start(filter_vis_t *fv, int start_time)
+{
+  fv->start_time = start_time;
+}
+
+static void insert_into_ipv4(peer_ipv4prefix_map_t *vis_map, uint16_t peer_id, bl_ipv4_pfx_t *prefix)
+{
+  int khret;
+  khiter_t k;
+  bl_ipv4_pfx_set_t *set;
+  if((k = kh_get(peer_ipv4prefix_map, vis_map, peer_id)) == kh_end(vis_map))
+    {
+      k = kh_put(peer_ipv4prefix_map, vis_map, peer_id, &khret);
+      kh_value(vis_map,k) = bl_ipv4_pfx_set_create();      
+    }
+  set = kh_value(vis_map,k);
+  assert(set != NULL);
+  bl_ipv4_pfx_set_insert(set,*prefix);  
+}
+
+static void insert_into_ipv6(peer_ipv6prefix_map_t *vis_map, uint16_t peer_id, bl_ipv6_pfx_t *prefix)
+{
+  int khret;
+  khiter_t k;
+  bl_ipv6_pfx_set_t *set;
+  if((k = kh_get(peer_ipv6prefix_map, vis_map, peer_id)) == kh_end(vis_map))
+    {
+      k = kh_put(peer_ipv6prefix_map, vis_map, peer_id, &khret);
+      kh_value(vis_map,k) = bl_ipv6_pfx_set_create();      
+    }
+  set = kh_value(vis_map,k);
+  assert(set != NULL);
+  bl_ipv6_pfx_set_insert(set,*prefix);  
+}
+
+static int filter_vis_update(filter_vis_t *fv, bgpstream_record_t *bs_record)
+{
+  
+  bgpstream_elem_t * bs_elem_queue;
+  bgpstream_elem_t * bs_iterator;
+  bl_addr_storage_t peer_ip; // TODO conversion
+  uint16_t peer_id;
+  bl_ipv4_pfx_t ipv4_prefix;
+  bl_ipv6_pfx_t ipv6_prefix;
+	  
+  if(bs_record->status == VALID_RECORD) 
+    {
+      bs_elem_queue = bgpstream_get_elem_queue(bs_record);
+      bs_iterator = bs_elem_queue;
+      while(bs_iterator != NULL)
+	{
+	  if(bs_iterator->type == BST_ANNOUNCEMENT || bs_iterator->type == BST_RIB)
+	    {
+	      // converting peer ip to storage
+	      if(bs_iterator->peer_address.type == BST_IPV4)
+		{
+		  peer_ip.version = BL_ADDR_IPV4;
+		  peer_ip.ipv4 = bs_iterator->peer_address.address.v4_addr;		  
+		}
+	      else
+		{
+		  peer_ip.version = BL_ADDR_IPV6;
+		  peer_ip.ipv6 = bs_iterator->peer_address.address.v6_addr;
+		}
+	      // getting peer_id	      
+	      peer_id = bl_peersign_map_set_and_get(fv->ps_map, bs_record->attributes.dump_collector, &peer_ip);
+
+	      if(bs_iterator->prefix.number.type == BST_IPV4)
+		{
+		  if(bs_iterator->prefix.len < fv->min_ipv4_mask_len || bs_iterator->prefix.len > fv->max_ipv4_mask_len)
+		    {
+		      continue;
+		    }
+		  
+		  ipv4_prefix.mask_len = bs_iterator->prefix.len;
+		  ipv4_prefix.address = bs_iterator->prefix.number.address.v4_addr;
+		  insert_into_ipv4(fv->ipv4_vis, peer_id, &ipv4_prefix);
+
+		}
+	      else
+		{
+		  if(bs_iterator->prefix.len > fv->max_ipv6_mask_len)
+		    {
+		      continue;
+		    }
+		  ipv6_prefix.mask_len = bs_iterator->prefix.len;
+		  ipv6_prefix.address = bs_iterator->prefix.number.address.v6_addr;
+		  insert_into_ipv6(fv->ipv6_vis, peer_id, &ipv6_prefix);
+		}	      
+	    }
+	}
+    }
+  return 0;  
+}
+
+
+static void insert_ipv4_pfxpeer_pair(ipv4prefix_peer_map_t *ipv4_pfx_visinfo, bl_ipv4_pfx_t *ipv4_pfx,
+			      uint16_t peer_id, bl_id_set_t *ipv4_full_feed)
+{
+  khiter_t k;
+  int khret;  
+  peer_breakdown_t pbd;
+  if((k = kh_get(ipv4prefix_peer_map, ipv4_pfx_visinfo, *ipv4_pfx)) == kh_end(ipv4_pfx_visinfo))
+    {
+      k = kh_put(ipv4prefix_peer_map, ipv4_pfx_visinfo, *ipv4_pfx, &khret);
+      pbd.full_feed_peers_cnt = 0;
+      pbd.all_peers_cnt = 0;
+      kh_value(ipv4_pfx_visinfo,k) = pbd;      
+    }
+  
+  pbd = kh_value(ipv4_pfx_visinfo,k);
+  // all peers cnt
+  pbd.all_peers_cnt++;
+  if(bl_id_set_exists(ipv4_full_feed, peer_id) == 1 )
+    {
+      // full feed peers count
+      pbd.full_feed_peers_cnt++;
+    }
+  kh_value(ipv4_pfx_visinfo,k) = pbd;      
+}
+
+
+static void insert_ipv6_pfxpeer_pair(ipv6prefix_peer_map_t *ipv6_pfx_visinfo, bl_ipv6_pfx_t *ipv6_pfx,
+			      uint16_t peer_id, bl_id_set_t *ipv6_full_feed)
+{
+  khiter_t k;
+  int khret;
+  peer_breakdown_t pbd;
+  if((k = kh_get(ipv6prefix_peer_map, ipv6_pfx_visinfo, *ipv6_pfx)) == kh_end(ipv6_pfx_visinfo))
+    {
+      k = kh_put(ipv6prefix_peer_map, ipv6_pfx_visinfo, *ipv6_pfx, &khret);
+      pbd.full_feed_peers_cnt = 0;
+      pbd.all_peers_cnt = 0;
+      kh_value(ipv6_pfx_visinfo,k) = pbd;      
+    }
+  
+  pbd = kh_value(ipv6_pfx_visinfo,k);
+  // all peers cnt
+  pbd.all_peers_cnt++;
+  if(bl_id_set_exists(ipv6_full_feed, peer_id) == 1 )
+    {
+      // full feed peers count
+      pbd.full_feed_peers_cnt++;
+    }
+  kh_value(ipv6_pfx_visinfo,k) = pbd;      
+}
+
+static void filter_vis_end(filter_vis_t *fv, int end_time)
+{
+  printf("END\n");
+  fv->end_time = end_time;
+
+  int khret;
+  khiter_t k;
+  uint16_t peer_id;
+  
+  bl_id_set_t *ipv4_full_feed = bl_id_set_create();
+  assert(ipv4_full_feed);
+
+  bl_id_set_t *ipv6_full_feed = bl_id_set_create();
+  assert(ipv6_full_feed);
+  
+  // Step 1: get full feed
+  
+  bl_ipv4_pfx_set_t *ipv4_set;
+  bl_ipv6_pfx_set_t *ipv6_set;
+
+  for (k = kh_begin(fv->ipv4_vis); k != kh_end(fv->ipv4_vis); ++k)
+    {
+      if (kh_exist(fv->ipv4_vis, k))
+	{
+	  peer_id = kh_key(fv->ipv4_vis, k);
+	  ipv4_set = kh_value(fv->ipv4_vis, k);
+	  if(kh_size(ipv4_set) > fv->ipv4_full_feed_th)
+	    {
+	      bl_id_set_insert(ipv4_full_feed, peer_id);
+	    }
+	}
+    }
+  
+  for (k = kh_begin(fv->ipv6_vis); k != kh_end(fv->ipv6_vis); ++k)
+    {
+      if (kh_exist(fv->ipv6_vis, k))
+	{
+	  peer_id = kh_key(fv->ipv6_vis, k);
+	  ipv6_set = kh_value(fv->ipv6_vis, k);
+	  if(kh_size(ipv6_set) > fv->ipv6_full_feed_th)
+	    {
+	      bl_id_set_insert(ipv6_full_feed, peer_id);
+	    }
+	}
+    }
+    
+  // Step 2: for each prefix count the number of peers (total and full)
+  ipv4prefix_peer_map_t *ipv4_pfx_visinfo = kh_init(ipv4prefix_peer_map);
+  ipv6prefix_peer_map_t *ipv6_pfx_visinfo = kh_init(ipv6prefix_peer_map);
+  bl_ipv4_pfx_t *ipv4_pfx;
+  bl_ipv6_pfx_t *ipv6_pfx;
+  khiter_t inn_k;
+  // fill ipv4 structure
+  for (k = kh_begin(fv->ipv4_vis); k != kh_end(fv->ipv4_vis); ++k)
+    {
+      if (kh_exist(fv->ipv4_vis, k))
+	{
+	  peer_id = kh_key(fv->ipv4_vis, k);
+	  ipv4_set = kh_value(fv->ipv4_vis, k);
+	  for (inn_k = kh_begin(ipv4_set); inn_k != kh_end(ipv4_set); ++inn_k)
+	    {
+	      if (kh_exist(ipv4_set, inn_k))
+		{
+		  ipv4_pfx = &kh_key(ipv4_set, inn_k);
+		  insert_ipv4_pfxpeer_pair(ipv4_pfx_visinfo, ipv4_pfx, peer_id, ipv4_full_feed);
+		}
+	    }
+	}
+    }
+  // fill ipv6 structure
+  for (k = kh_begin(fv->ipv6_vis); k != kh_end(fv->ipv6_vis); ++k)
+    {
+      if (kh_exist(fv->ipv6_vis, k))
+	{
+	  peer_id = kh_key(fv->ipv6_vis, k);
+	  ipv6_set = kh_value(fv->ipv6_vis, k);
+	  for (inn_k = kh_begin(ipv6_set); inn_k != kh_end(ipv6_set); ++inn_k)
+	    {
+	      if (kh_exist(ipv6_set, inn_k))
+		{
+		  ipv6_pfx = &kh_key(ipv6_set, inn_k);
+		  insert_ipv6_pfxpeer_pair(ipv6_pfx_visinfo, ipv6_pfx, peer_id, ipv6_full_feed);
+		}
+	    }
+	}
+    }
+
+  // Step 3: print results
+  peer_breakdown_t pbd;
+
+  // printing ipv4 prefixes (all)
+  for (k = kh_begin(ipv4_pfx_visinfo); k != kh_end(ipv4_pfx_visinfo); ++k)
+    {
+      if (kh_exist(ipv4_pfx_visinfo, k))
+	{
+	  ipv4_pfx = &kh_key(ipv4_pfx_visinfo, k);
+	  pbd = kh_value(ipv4_pfx_visinfo, k);
+	  // print
+	  printf("%d/%d\t%u\t%u\n", ipv4_pfx->address.s_addr,
+		 ipv4_pfx->mask_len, pbd.full_feed_peers_cnt, pbd.all_peers_cnt);
+	}
+    }
+
+  // printing ipv6 prefixes (all)
+  for (k = kh_begin(ipv6_pfx_visinfo); k != kh_end(ipv6_pfx_visinfo); ++k)
+    {
+      if (kh_exist(ipv6_pfx_visinfo, k))
+	{
+	  ipv6_pfx = &kh_key(ipv6_pfx_visinfo, k);
+	  pbd = kh_value(ipv6_pfx_visinfo, k);
+	  // print
+	}
+    }
+
+  
+
+  // TODO: destroy tmp structures
+  // TODO: reset persistent structure
+}
+
+static void filter_vis_destroy(filter_vis_t *fv)
+{
+  if(fv != NULL)
+    {
+      if(fv->ps_map != NULL)
+	{
+	  bl_peersign_map_destroy(fv->ps_map);
+	  fv->ps_map = NULL;
+	}
+      free(fv);
+    }
+
+  // TODO: destroy other objects
+}
+
+/** ************************************************** */
 
 
 /** Holds the state for an instance of this plugin */
@@ -204,10 +573,10 @@ int bgpcorsaro_filtervis_init_output(bgpcorsaro_t *bgpcorsaro)
 #endif
 
   /** plugin initialization */
-  if((state->filter_vis = (filter_vis_t *)malloc_zero(sizeof(filter_vis_t *))) == NULL)
+  if((state->filter_vis = filter_vis_create()) == NULL)
    {
       bgpcorsaro_log(__func__, bgpcorsaro,
-		     "could not create bgpribs in bgpcorsaro_filtervis_state_t");
+		     "could not create filter_vis in bgpcorsaro_filtervis_state_t");
       goto err;
     }
   
@@ -274,8 +643,7 @@ int bgpcorsaro_filtervis_start_interval(bgpcorsaro_t *bgpcorsaro,
     }
 
   /** plugin interval start operations */
-  // TODO
-  // bgpribs_interval_start(state->filter_vis, int_start->time);
+  filter_vis_start(state->filter_vis, int_start->time);
 
   bgpcorsaro_io_write_interval_start(bgpcorsaro, state->outfile, int_start);
 
@@ -292,15 +660,8 @@ int bgpcorsaro_filtervis_end_interval(bgpcorsaro_t *bgpcorsaro,
   bgpcorsaro_log(__func__, bgpcorsaro, "Dumping stats for interval %d",
 		 int_end->number);
 
-  /** plugin end of interval operations */
-  // TODO
-  /* if(bgpribs_interval_end(state->filter_vis, int_end->time) < 0) */
-  /*   { */
-  /*     // an error occurred during the interval_end operations */
-  /*     bgpcorsaro_log(__func__, bgpcorsaro, "could not dump stats for %s plugin", */
-  /* 		     PLUGIN(bgpcorsaro)->name);       */
-  /*     return -1; */
-  /*   } */
+  /** plugin interval end operations */
+  filter_vis_end(state->filter_vis, int_end->time);
 
   bgpcorsaro_io_write_interval_end(bgpcorsaro, state->outfile, int_end);
 
@@ -344,10 +705,8 @@ int bgpcorsaro_filtervis_process_record(bgpcorsaro_t *bgpcorsaro,
   assert(record != NULL);
   bgpstream_record_t * bs_record = BS_REC(record);
   assert(bs_record != NULL);
+  
   /** plugin operations related to a single record*/
-// TODO
-  // return bgpribs_process_record(state->filter_vis, bs_record);
-return 0;
-
+  return filter_vis_update(state->filter_vis, bs_record);
 }
 
