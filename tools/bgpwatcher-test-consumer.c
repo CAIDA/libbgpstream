763670115f84e0a817d28179a8abd0150e4774d0
Alistair King <alistair@caida.org>
2014-12-09 08:47:15 -0800

    add consumer test driver

---
+++ tools/bgpwatcher-test-consumer.c
@@ -0,0 +1,255 @@
+/*
+ * bgpwatcher
+ *
+ * Alistair King, CAIDA, UC San Diego
+ * corsaro-info@caida.org
+ *
+ * Copyright (C) 2012 The Regents of the University of California.
+ *
+ * This file is part of bgpwatcher.
+ *
+ * bgpwatcher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bgpwatcher is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with bgpwatcher.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "config.h"
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>       /* time */
+
+/* this must be all we include from bgpwatcher */
+#include <bgpwatcher_client.h>
+
+#include "config.h"
+#include "utils.h"
+
+static void usage(const char *name)
+{
+  fprintf(stderr,
+	  "usage: %s [<options>]\n"
+	  "       -i <interval-ms>      Time in ms between heartbeats to server\n"
+	  "                               (default: %d)\n"
+          "       -I <interest>         Advertise the given interest. May be used multiple times\n"
+          "                               One of: first-full, full, partial\n"
+	  "       -l <beats>            Number of heartbeats that can go by before the\n"
+	  "                               server is declared dead (default: %d)\n"
+	  "       -n <identity>         Globally unique client name (default: random)\n"
+	  "       -r <retry-min>        Min wait time (in msec) before reconnecting server\n"
+
+	  "                               (default: %d)\n"
+	  "       -R <retry-max>        Max wait time (in msec) before reconnecting server\n"
+	  "                               (default: %d)\n"
+	  "       -s <server-uri>       0MQ-style URI to connect to server on\n"
+	  "                               (default: %s)\n"
+          "       -s <server-sub-uri>   0MQ-style URI to subscribe to tables on\n"
+          "                               (default: %s)\n",
+	  name,
+	  BGPWATCHER_HEARTBEAT_INTERVAL_DEFAULT,
+	  BGPWATCHER_HEARTBEAT_LIVENESS_DEFAULT,
+	  BGPWATCHER_RECONNECT_INTERVAL_MIN,
+	  BGPWATCHER_RECONNECT_INTERVAL_MAX,
+	  BGPWATCHER_CLIENT_SERVER_URI_DEFAULT,
+	  BGPWATCHER_CLIENT_SERVER_SUB_URI_DEFAULT);
+}
+
+int main(int argc, char **argv)
+{
+  /* for option parsing */
+  int opt;
+  int prevoptind;
+
+  /* to store command line argument values */
+  const char *server_uri = NULL;
+  const char *server_sub_uri = NULL;
+  const char *identity = NULL;
+
+  uint64_t heartbeat_interval = BGPWATCHER_HEARTBEAT_INTERVAL_DEFAULT;
+  int heartbeat_liveness      = BGPWATCHER_HEARTBEAT_LIVENESS_DEFAULT;
+  uint64_t reconnect_interval_min = BGPWATCHER_RECONNECT_INTERVAL_MIN;
+  uint64_t reconnect_interval_max = BGPWATCHER_RECONNECT_INTERVAL_MAX;
+
+  uint8_t interests = 0;
+  uint8_t intents = 0;
+  bgpwatcher_client_t *client = NULL;
+
+  bgpwatcher_consumer_interest_t rx_interest;
+  bgpwatcher_view_t view;
+
+  while(prevoptind = optind,
+	(opt = getopt(argc, argv, ":i:l:n:r:R:s:S:v?")) >= 0)
+    {
+      if (optind == prevoptind + 2 && *optarg == '-' ) {
+        opt = ':';
+        -- optind;
+      }
+      switch(opt)
+	{
+	case ':':
+	  fprintf(stderr, "ERROR: Missing option argument for -%c\n", optopt);
+	  usage(argv[0]);
+	  return -1;
+	  break;
+
+	case 'i':
+	  heartbeat_interval = atoi(optarg);
+	  break;
+
+        case 'I':
+          if(strcmp(optarg, "first-full") == 0)
+            {
+              interests |= BGPWATCHER_CONSUMER_INTEREST_FIRSTFULL;
+            }
+          else if(strcmp(optarg, "full") == 0)
+            {
+              interests |= BGPWATCHER_CONSUMER_INTEREST_FULL;
+            }
+          else if(strcmp(optarg, "partial") == 0)
+            {
+              interests |= BGPWATCHER_CONSUMER_INTEREST_PARTIAL;
+            }
+          else
+            {
+              fprintf(stderr,
+                      "ERROR: Invalid interest (%s)."
+                      "Interest must be one of "
+                      "'first-full', 'full', or 'partial'\n", optarg);
+              usage(argv[0]);
+              return -1;
+            }
+          break;
+
+	case 'l':
+	  heartbeat_liveness = atoi(optarg);
+	  break;
+
+	case 'n':
+	  identity = optarg;
+	  break;
+
+	case 'r':
+	  reconnect_interval_min = atoi(optarg);
+	  break;
+
+	case 'R':
+	  reconnect_interval_max = atoi(optarg);
+	  break;
+
+	case 's':
+	  server_uri = optarg;
+	  break;
+
+	case 'S':
+	  server_sub_uri = optarg;
+	  break;
+
+	case '?':
+	case 'v':
+	  fprintf(stderr, "bgpwatcher version %d.%d.%d\n",
+		  BGPWATCHER_MAJOR_VERSION,
+		  BGPWATCHER_MID_VERSION,
+		  BGPWATCHER_MINOR_VERSION);
+	  usage(argv[0]);
+	  return 0;
+	  break;
+
+	default:
+	  usage(argv[0]);
+	  return -1;
+	  break;
+	}
+    }
+
+  /* NB: once getopt completes, optind points to the first non-option
+     argument */
+
+  if(interests == 0)
+    {
+      fprintf(stderr, "WARN: Defaulting to FULL interest\n");
+      interests = BGPWATCHER_CONSUMER_INTEREST_FULL;
+    }
+
+  if((client =
+      bgpwatcher_client_init(interests, intents)) == NULL)
+    {
+      fprintf(stderr, "ERROR: could not initialize bgpwatcher client\n");
+      usage(argv[0]);
+      goto err;
+    }
+
+  if(server_uri != NULL &&
+     bgpwatcher_client_set_server_uri(client, server_uri) != 0)
+    {
+      bgpwatcher_client_perr(client);
+      goto err;
+    }
+
+  if(server_sub_uri != NULL &&
+     bgpwatcher_client_set_server_sub_uri(client, server_sub_uri) != 0)
+    {
+      bgpwatcher_client_perr(client);
+      goto err;
+    }
+
+  if(identity != NULL &&
+     bgpwatcher_client_set_identity(client, identity) != 0)
+    {
+      bgpwatcher_client_perr(client);
+      goto err;
+    }
+
+  bgpwatcher_client_set_heartbeat_interval(client, heartbeat_interval);
+
+  bgpwatcher_client_set_heartbeat_liveness(client, heartbeat_liveness);
+
+  bgpwatcher_client_set_reconnect_interval_min(client, reconnect_interval_min);
+
+  bgpwatcher_client_set_reconnect_interval_max(client, reconnect_interval_max);
+
+  fprintf(stderr, "TEST: Starting client... ");
+  if(bgpwatcher_client_start(client) != 0)
+    {
+      bgpwatcher_client_perr(client);
+      goto err;
+    }
+  fprintf(stderr, "done\n");
+
+  while(bgpwatcher_client_recv_view(client, BGPWATCHER_CLIENT_RECV_MODE_BLOCK,
+				    &rx_interest, &view) == 0)
+    {
+      bgpwatcher_view_dump(&view);
+    }
+
+  fprintf(stderr, "TEST: Shutting down...\n");
+
+  bgpwatcher_client_stop(client);
+  bgpwatcher_client_perr(client);
+
+  /* cleanup */
+  bgpwatcher_client_free(client);
+  fprintf(stderr, "TEST: Shutdown complete\n");
+
+  /* complete successfully */
+  return 0;
+
+ err:
+  bgpwatcher_client_perr(client);
+  if(client != NULL) {
+    bgpwatcher_client_free(client);
+  }
+  return -1;
+}
