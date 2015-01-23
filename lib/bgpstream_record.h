f10db867a8c66b7b98d2e0e7a1caf9108f87474a
Alistair King <alistair@caida.org>
2015-01-23 10:27:20 -0800

    cleanup bgpstream_record.h and add docs

--- lib/bgpstream_record.h
+++ lib/bgpstream_record.h
@@ -23,24 +23,56 @@
  *
  */
 
-#ifndef _BGPSTREAM_RECORD_H
-#define _BGPSTREAM_RECORD_H
+#ifndef __BGPSTREAM_RECORD_H
+#define __BGPSTREAM_RECORD_H
 
-#include <stdbool.h>
+/** @file
+ *
+ * @brief Header file that exposes the public interface of a bgpstream record.
+ *
+ * @author Chiara Orsini
+ *
+ */
 
-
-#define BGPSTREAM_PAR_LEN 512
+/**
+ * @name Public Opaque Data Structures
+ *
+ * @{ */
 
 /** An opaque pointer to a BGPDUMP_ENTRY field.
  *
- * @note this is an *internal* datastructure, it is *not* guaranteed that it
+ * @note this is an *internal* data structure, it is *not* guaranteed that it
  * will continue to conform to the public BGPDUMP_ENTRY structure. This is NOT
  * for use in user code
  */
 typedef struct struct_BGPDUMP_ENTRY bgpstream_record_mrt_data_t;
 
-typedef enum {BGPSTREAM_UPDATE = 0,
-	      BGPSTREAM_RIB    = 1
+/** @} */
+
+/**
+ * @name Public Constants
+ *
+ * @{ */
+
+/** The maximum number of characters in a string attribute */
+#define BGPSTREAM_PAR_LEN 512
+
+/** @} */
+
+/**
+ * @name Public Enums
+ *
+ * @{ */
+
+/** The type of the record */
+typedef enum {
+
+  /** The record contains data for a BGP Update message */
+  BGPSTREAM_UPDATE = 0,
+
+  /** The record contains data for a BGP RIB message */
+  BGPSTREAM_RIB    = 1,
+
 } bgpstream_record_dump_type_t;
 
 typedef enum {DUMP_START  = 0,        /* first entry in dump */
@@ -48,41 +80,102 @@ typedef enum {DUMP_START  = 0,        /* first entry in dump */
 	      DUMP_END    = 2       /* last entry in dump */
 } bgpstream_dump_position_t;
 
-typedef enum {VALID_RECORD     = 0,    /* valid entry found in dump */
-	      FILTERED_SOURCE  = 1,    /* fltered source: source is not empty, but no valid record found */
-	      EMPTY_SOURCE     = 2,   /* empty source: source has no entries */
-	      CORRUPTED_SOURCE = 3,   /* corrupted source: error in opening dump */
-	      CORRUPTED_RECORD = 4    /* corrupted record: dump corrupted at some point */
+/** Status of the record */
+typedef enum {
+
+  /** The record is valid and may be used */
+  VALID_RECORD     = 0,
+
+  /** Source is not empty, but no valid record was found */
+  FILTERED_SOURCE  = 1,
+
+  /** Source has no entries */
+  EMPTY_SOURCE     = 2,
+
+  /* Error in opening dump */
+  CORRUPTED_SOURCE = 3,
+
+  /* Dump corrupted at some point */
+  CORRUPTED_RECORD = 4,
 } bgpstream_record_status_t;
 
-#define BGPSTREAM_RECORD_TYPE_MAX 5
+/** @} */
 
+/**
+ * @name Public Data Structures
+ *
+ * @{ */
+
+/** Record attributes */
 typedef struct struct_bgpstream_record_attributes_t {
-  // define a list of useful attributes to associate to bgp dump entry
-  char dump_project[BGPSTREAM_PAR_LEN];    // project name
-  char dump_collector[BGPSTREAM_PAR_LEN];  // collector name
-  bgpstream_record_dump_type_t dump_type;  // dump type
-  long dump_time;   // timestamp associated with the time the bgp data was "aggregated"
-  long record_time; // timestamp associated with the time the bgp data was last seen
+
+  /** Project name */
+  char dump_project[BGPSTREAM_PAR_LEN];
+
+  /** Collector name */
+  char dump_collector[BGPSTREAM_PAR_LEN];
+
+  /** Dump type */
+  bgpstream_record_dump_type_t dump_type;
+
+  /** Time that the BGP data was "aggregated". E.g. the start time of an MRT
+      dump file */
+  long dump_time;
+
+  /** Time from the MRT record. I.e. the time *this* record was dumped */
+  long record_time;
+
 } bgpstream_record_attributes_t;
 
 
+/** Record structure */
 typedef struct struct_bgpstream_record_t {
+
+  /** INTERNAL: buffer containing the underlying MRT data for the record */
   bgpstream_record_mrt_data_t *bd_entry;
+
+  /** Collection of attributes pertaining to this record */
   bgpstream_record_attributes_t attributes;
+
+  /** Status of this record */
   bgpstream_record_status_t status;
+
+  /** Position of this record in the dump */
   bgpstream_dump_position_t dump_pos;
+
 } bgpstream_record_t;
 
-/* allocate memory for a bs_record (the client can refer to this
- * memory, however, if it has to save this object, it needs to
- * copy the memory itself) */
+/** @} */
+
+/**
+ * @name Public API Functions
+ *
+ * @{ */
+
+/** Create a new BGP Stream Record instance for passing to
+ * bgpstream_get_next_record.
+ *
+ * @return a pointer to a Record instance if successful, NULL otherwise
+ *
+ * A Record may be reused for successive calls to bgpstream_get_next_record if
+ * records are processed independently of each other
+ */
 bgpstream_record_t *bgpstream_record_create();
 
-/* deallocate memory for the bs_record */
-void bgpstream_record_destroy(bgpstream_record_t * const bs_record);
+/** Destroy the given BGP Stream Record instance
+ *
+ * @param record        pointer to a BGP Stream Record instance to destroy
+ */
+void bgpstream_record_destroy(bgpstream_record_t *record);
 
-/* wrapper around bgpdump_print_entry */
+/** Dump the given record to stdout in bgpdump format
+ *
+ * @param record        pointer to a BGP Stream Record instance to dump
+ *
+ * See https://bitbucket.org/ripencc/bgpdump for more information about bgpdump
+ */
 void bgpstream_record_print_mrt_data(bgpstream_record_t * const bs_record);
 
-#endif /* _BGPSTREAM_RECORD_H */
+/** @} */
+
+#endif /* __BGPSTREAM_RECORD_H */
