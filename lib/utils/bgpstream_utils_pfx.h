eccf2f74b9186a1fcef05d8f4c8ed9c1bc09b111
Chiara Orsini <chiara@caida.org>
2015-12-03 17:22:12 -0800

    peer ASNs and prefixes filters added

--- lib/utils/bgpstream_utils_pfx.h
+++ lib/utils/bgpstream_utils_pfx.h
@@ -208,7 +208,7 @@ int bgpstream_pfx_storage_equal(bgpstream_pfx_storage_t *pfx1,
  *         prefix is not valid
  */
 bgpstream_pfx_storage_t *
-bgpstream_str2pfx(char *pfx_str, bgpstream_pfx_storage_t *pfx);
+bgpstream_str2pfx(const char *pfx_str, bgpstream_pfx_storage_t *pfx);
 
 /** @} */
 
