1002bc8480b5e25c7e1790ed33fbab0922a3129a
Chiara Orsini <chiara@caida.org>
2015-12-04 13:47:25 -0800

    fix compiler related bugs

--- lib/bgpstream_record.h
+++ lib/bgpstream_record.h
@@ -48,8 +48,6 @@
  */
 typedef struct struct_BGPDUMP_ENTRY bgpstream_record_mrt_data_t;
 
-typedef struct struct_bgpstream_t bgpstream_t;
-
 /** @} */
 
 /**
@@ -143,7 +141,7 @@ typedef struct struct_bgpstream_record_attributes_t {
 typedef struct struct_bgpstream_record_t {
 
   /** INTERNAL: pointer to the originating bgpstream instance */
-  bgpstream_t *bs;
+  struct struct_bgpstream_t *bs;
 
   /** INTERNAL: buffer containing the underlying MRT data for the record */
   bgpstream_record_mrt_data_t *bd_entry;
