48a4773bb3766bbef82fababb729a306b8676e3f
Alistair King <alistair@caida.org>
2017-08-29 15:23:54 -0700

    Refactor even more common parsebgp code out of the MRT format

--- lib/formats/bs_format_mrt.c
+++ lib/formats/bs_format_mrt.c
@@ -31,12 +31,6 @@
 #include <assert.h>
 
 #define STATE ((state_t*)(format->state))
-#define FDATA ((parsebgp_msg_t*)(record->__format_data->data))
-
-// read in chunks of 1MB to minimize the number of partial parses we end up
-// doing.  this is also the same length as the wandio thread buffer, so this
-// might help reduce the time waiting for locks
-#define BUFLEN 1024 * 1024
 
 typedef struct peer_index_entry {
 
@@ -53,28 +47,11 @@ KHASH_INIT(td2_peer, int, peer_index_entry_t, 1, kh_int_hash_func,
 
 typedef struct state {
 
-  // options for libparsebgp
-  parsebgp_opts_t parser_opts;
-
-  // raw data buffer
-  // TODO: once parsebgp supports reading using a read callback, just pass the
-  // transport callback to the parser
-  uint8_t buffer[BUFLEN];
-
-  // number of bytes left to read in the buffer
-  size_t remain;
-
-  // pointer into buffer
-  uint8_t *ptr;
+  // parsebgp decode wrapper state
+  bgpstream_parsebgp_decode_state_t decoder;
 
   // elem generator instance
   bgpstream_elem_generator_t *gen;
-
-  // the total number of successful (filtered and not) reads
-  uint64_t successful_read_cnt;
-
-  // the number of non-filtered reads (i.e. "useful")
-  uint64_t valid_read_cnt;
 
   // state to store the "peer index table" when reading TABLE_DUMP_V2 records
   khash_t(td2_peer) *peer_table;
@@ -532,62 +509,37 @@ static int is_wanted_time(uint32_t record_time,
   return 0;
 }
 
-static ssize_t refill_buffer(bgpstream_format_t *format, uint8_t *buf,
-                             size_t buflen, size_t remain)
+static int populate_filter_cb(bgpstream_format_t *format,
+                              parsebgp_msg_t *msg)
 {
-  size_t len = 0;
-  int64_t new_read = 0;
+  assert(msg->type == PARSEBGP_MSG_TYPE_MRT);
 
-  if (remain > 0) {
-    // need to move remaining data to start of buffer
-    memmove(buf, buf + buflen - remain, remain);
-    len += remain;
+  // if this is a peer index table message, we parse it now and move on (we
+  // could also add a "filtered" flag to the peer_index_entry_t struct so that
+  // when elem parsing happens it can quickly filter out unwanted peers
+  // without having to check ASN or IP
+  if (msg->types.mrt.type == PARSEBGP_MRT_TYPE_TABLE_DUMP_V2 &&
+      msg->types.mrt.subtype ==
+      PARSEBGP_MRT_TABLE_DUMP_V2_PEER_INDEX_TABLE) {
+    if (handle_td2_peer_index(
+          format, &msg->types.mrt.types.table_dump_v2.peer_index) != 0) {
+      bgpstream_log(BGPSTREAM_LOG_ERR, "Failed to process Peer Index Table");
+      return -1;
+    }
+    // indicate that we want this message skipped
+    return 0;
   }
 
-  // try and do a read
-  if ((new_read = bgpstream_transport_read(format->transport, buf + len,
-                                           buflen - len)) < 0) {
-    // read failed
-    return new_read;
+  // check the filters
+  // TODO: if this is a BGP4MP or TD1 message (UPDATE), then we can do some
+  // work to prep the path attributes (and then filter on them).
+
+  if (is_wanted_time(msg->types.mrt.timestamp_sec, format->filter_mgr) != 0) {
+    // we want this entry
+    return 1;
+  } else {
+    return 0;
   }
-
-  // new_read could be 0, indicating EOF, so need to check returned len is
-  // larger than passed in remain
-  return len + new_read;
-}
-
-static bgpstream_format_status_t handle_eof(bgpstream_format_t *format,
-                                            bgpstream_record_t *record,
-                                            uint64_t skipped_cnt)
-{
-  assert(FDATA == NULL);
-
-  // just to be kind, set the record time to the dump time
-  record->attributes.record_time = record->attributes.dump_time;
-
-  if (skipped_cnt == 0) {
-    // signal that the previous record really was the last in the dump
-    record->dump_pos = BGPSTREAM_DUMP_END;
-  }
-  // was this the first thing we tried to read?
-  if (STATE->successful_read_cnt == 0) {
-    // then it is an empty file
-    record->status = BGPSTREAM_RECORD_STATUS_EMPTY_SOURCE;
-    record->dump_pos = BGPSTREAM_DUMP_END;
-    return BGPSTREAM_FORMAT_EMPTY_DUMP;
-  }
-
-  // so we managed to read some things, but did we get anything useful from
-  // this file?
-  if (STATE->valid_read_cnt == 0) {
-    // dump contained data, but we filtered all of them out
-    record->status = BGPSTREAM_RECORD_STATUS_FILTERED_SOURCE;
-    record->dump_pos = BGPSTREAM_DUMP_END;
-    return BGPSTREAM_FORMAT_FILTERED_DUMP;
-  }
-
-  // otherwise, signal end of dump (record has not been filled)
-  return BGPSTREAM_FORMAT_END_OF_DUMP;
 }
 
 /* ==================== PUBLIC API BELOW HERE ==================== */
@@ -606,30 +558,9 @@ int bs_format_mrt_create(bgpstream_format_t *format,
     return -1;
   }
 
-  opts = &STATE->parser_opts;
+  opts = &STATE->decoder.parser_opts;
   parsebgp_opts_init(opts);
-
-  // select only the Path Attributes that we care about
-  opts->bgp.path_attr_filter_enabled = 1;
-  opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_ORIGIN] = 1;
-  opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_AS_PATH] = 1;
-  opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_NEXT_HOP] = 1;
-  //opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_MED] = 1;
-  //opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_LOCAL_PREF] = 1;
-  //opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_ATOMIC_AGGREGATE] = 1;
-  //opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_AGGREGATOR] = 1;
-  opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_COMMUNITIES] = 1;
-  opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_MP_REACH_NLRI] = 1;
-  opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_MP_UNREACH_NLRI] = 1;
-  opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_AS4_PATH] = 1;
-  //opts->bgp.path_attr_filter[PARSEBGP_BGP_PATH_ATTR_TYPE_AS4_AGGREGATOR] = 1;
-
-  // and ask for shallow parsing of communities
-  opts->bgp.path_attr_raw_enabled = 1;
-  opts->bgp.path_attr_raw[PARSEBGP_BGP_PATH_ATTR_TYPE_COMMUNITIES] = 1;
-
-  // DEBUG: (switch to ignore in production)
-  opts->ignore_not_implemented = 0;
+  bgpstream_parsebgp_opts_init(opts);
 
   return 0;
 }
@@ -638,143 +569,8 @@ bgpstream_format_status_t
 bs_format_mrt_populate_record(bgpstream_format_t *format,
                               bgpstream_record_t *record)
 {
-  assert(record->__format_data->format == format);
-  assert(FDATA == NULL);
-
-  int refill = 0;
-  ssize_t fill_len = 0;
-  size_t dec_len = 0;
-  uint64_t skipped_cnt = 0;
-  parsebgp_error_t err;
-
- refill:
-  if (STATE->remain == 0 || refill != 0) {
-    // try to refill the buffer
-    if ((fill_len =
-         refill_buffer(format, STATE->buffer, BUFLEN, STATE->remain)) == 0) {
-      // EOF
-      return handle_eof(format, record, skipped_cnt);
-    }
-    if (fill_len < 0) {
-      // read error
-      // TODO: create a specific read error failure so that perhaps BGPStream
-      // can retry
-      return -1;
-    }
-    if (fill_len == STATE->remain) {
-      bgpstream_log(BGPSTREAM_LOG_WARN,
-                    "DEBUG: Corrupted dump or failed read\n");
-      return BGPSTREAM_FORMAT_CORRUPTED_DUMP;
-    }
-    // here we have something new to read
-    STATE->remain = fill_len;
-    STATE->ptr = STATE->buffer;
-  }
-
-  while (STATE->remain > 0) {
-    if (FDATA == NULL &&
-        (record->__format_data->data = parsebgp_create_msg()) == NULL) {
-      bgpstream_log(BGPSTREAM_LOG_ERR,
-                    "ERROR: Failed to create message structure\n");
-      return -1;
-    }
-
-    dec_len = STATE->remain;
-    if ((err = parsebgp_decode(STATE->parser_opts, PARSEBGP_MSG_TYPE_MRT,
-                               FDATA, STATE->ptr, &dec_len)) != PARSEBGP_OK) {
-      if (err == PARSEBGP_PARTIAL_MSG) {
-        // refill the buffer and try again
-        refill = 1;
-        parsebgp_destroy_msg(FDATA);
-        record->__format_data->data = NULL;
-        goto refill;
-      }
-      // else: its a fatal error
-      bgpstream_log(BGPSTREAM_LOG_ERR,
-                    "ERROR: Failed to parse message (%d:%s)\n", err,
-                    parsebgp_strerror(err));
-      parsebgp_destroy_msg(FDATA);
-      record->__format_data->data = NULL;
-      return BGPSTREAM_FORMAT_CORRUPTED_DUMP;
-    }
-    // else: successful read
-    STATE->ptr += dec_len;
-    STATE->remain -= dec_len;
-
-    // got a message!
-    assert(FDATA != NULL);
-    assert(FDATA->type == PARSEBGP_MSG_TYPE_MRT);
-
-    // if this is a peer index table message, we parse it now and move on (we
-    // could also add a "filtered" flag to the peer_index_entry_t struct so that
-    // when elem parsing happens it can quickly filter out unwanted peers
-    // without having to check ASN or IP
-
-    if (FDATA->types.mrt.type == PARSEBGP_MRT_TYPE_TABLE_DUMP_V2 &&
-        FDATA->types.mrt.subtype ==
-          PARSEBGP_MRT_TABLE_DUMP_V2_PEER_INDEX_TABLE) {
-      if (handle_td2_peer_index(
-            format, &FDATA->types.mrt.types.table_dump_v2.peer_index) != 0) {
-        bgpstream_log(BGPSTREAM_LOG_ERR, "Failed to process Peer Index Table");
-        parsebgp_destroy_msg(FDATA);
-        record->__format_data->data = NULL;
-        return BGPSTREAM_FORMAT_CORRUPTED_DUMP;
-      }
-      // move on to the next record
-      parsebgp_destroy_msg(FDATA);
-      record->__format_data->data = NULL;
-      continue;
-    }
-
-    STATE->successful_read_cnt++;
-
-    // check the filters
-    // TODO: if this is a BGP4MP or TD1 message (UPDATE), then we can do some
-    // work to prep the path attributes (and then filter on them).
-
-    if (is_wanted_time(FDATA->types.mrt.timestamp_sec, format->filter_mgr) !=
-        0) {
-      // we want this entry
-      STATE->valid_read_cnt++;
-      break;
-    } else {
-      // we dont want this entry, destroy it
-      parsebgp_destroy_msg(FDATA);
-      record->__format_data->data = NULL;
-      if (skipped_cnt == UINT64_MAX) {
-        // probably this will never happen, but lets just be careful we don't
-        // wrap and think we haven't skipped anything
-        skipped_cnt = 0;
-      }
-      skipped_cnt++;
-      // fall through and repeat loop
-    }
-  }
-
-  if (STATE->remain == 0 && FDATA == NULL) {
-    // EOF
-    return handle_eof(format, record, skipped_cnt);
-  }
-
-  // valid message, and it passes our filters
-  record->status = BGPSTREAM_RECORD_STATUS_VALID_RECORD;
-
-  // if this is the first record we read and no previous
-  // valid record has been discarded because of time
-  if (STATE->valid_read_cnt == 1 && STATE->successful_read_cnt == 1) {
-    record->dump_pos = BGPSTREAM_DUMP_START;
-  } else {
-    record->dump_pos = BGPSTREAM_DUMP_MIDDLE;
-    // NB when the *next* record is pre-fetched, this may be changed to
-    // end-of-dump by the reader (since we'll discover that there are no more
-    // records)
-  }
-
-  // update the record time
-  record->attributes.record_time = FDATA->types.mrt.timestamp_sec;
-
-  // we successfully read a record, return it
-  return BGPSTREAM_FORMAT_OK;
+  return bgpstream_parsebgp_populate_record(&STATE->decoder, format, record,
+                                            populate_filter_cb);
 }
 
 int bs_format_mrt_get_next_elem(bgpstream_format_t *format,
@@ -782,7 +578,7 @@ int bs_format_mrt_get_next_elem(bgpstream_format_t *format,
                                 bgpstream_elem_t **elem)
 {
   if (bgpstream_elem_generator_is_populated(STATE->gen) == 0 &&
-      populate_elem_generator(format, FDATA) != 0) {
+      populate_elem_generator(format, BGPSTREAM_PARSEBGP_FDATA) != 0) {
     bgpstream_log(BGPSTREAM_LOG_ERR, "Elem generator population failed");
     return -1;
   }
