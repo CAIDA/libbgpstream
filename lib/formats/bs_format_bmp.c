3f2b5a3ecce0377c89ac922755e5347d3e85a67c
Alistair King <alistair@caida.org>
2017-08-31 17:59:00 -0700

    Switch to using per-record elem generators

--- lib/formats/bs_format_bmp.c
+++ lib/formats/bs_format_bmp.c
@@ -31,10 +31,9 @@
 
 #define STATE ((state_t*)(format->state))
 
-typedef struct state {
+#define RDATA ((rec_data_t *)(record->__format_data->data))
 
-  // parsebgp decode wrapper state
-  bgpstream_parsebgp_decode_state_t decoder;
+typedef struct rec_data {
 
   // reusable elem instance
   bgpstream_elem_t *elem;
@@ -48,38 +47,46 @@ typedef struct state {
   // state for UPDATE elem extraction
   bgpstream_parsebgp_upd_state_t upd_state;
 
+  // reusable parser message structure
+  parsebgp_msg_t *msg;
+
+} rec_data_t;
+
+typedef struct state {
+
+  // parsebgp decode wrapper state
+  bgpstream_parsebgp_decode_state_t decoder;
+
 } state_t;
 
-static int handle_update(bgpstream_format_t *format, parsebgp_bgp_msg_t *bgp)
+static int handle_update(rec_data_t *rd, parsebgp_bgp_msg_t *bgp)
 {
   int rc;
 
-  if ((rc = bgpstream_parsebgp_process_update(&STATE->upd_state, STATE->elem,
+  if ((rc = bgpstream_parsebgp_process_update(&rd->upd_state, rd->elem,
                                               bgp)) < 0) {
     return rc;
   }
   if (rc == 0) {
-    STATE->end_of_elems = 1;
+    rd->end_of_elems = 1;
   }
   return rc;
 }
 
-static int handle_peer_up_down(bgpstream_format_t *format, int peer_up)
+static int handle_peer_up_down(rec_data_t *rd, int peer_up)
 {
-  bgpstream_elem_t *el = STATE->elem;
-
-  el->type = BGPSTREAM_ELEM_TYPE_PEERSTATE;
+  rd->elem->type = BGPSTREAM_ELEM_TYPE_PEERSTATE;
 
   // TODO: fix this after talking with Tim
   // it is possible we can assume UP means IDLE->ACTIVE
-  el->old_state = BGPSTREAM_ELEM_PEERSTATE_UNKNOWN;
+  rd->elem->old_state = BGPSTREAM_ELEM_PEERSTATE_UNKNOWN;
   if (peer_up) {
-    el->new_state = BGPSTREAM_ELEM_PEERSTATE_ACTIVE;
+    rd->elem->new_state = BGPSTREAM_ELEM_PEERSTATE_ACTIVE;
   } else {
-    el->new_state = BGPSTREAM_ELEM_PEERSTATE_IDLE;
+    rd->elem->new_state = BGPSTREAM_ELEM_PEERSTATE_IDLE;
   }
 
-  STATE->end_of_elems = 1;
+  rd->end_of_elems = 1;
   return 1;
 }
 
@@ -98,14 +105,6 @@ static int handle_peer_hdr(bgpstream_elem_t *el, parsebgp_bmp_msg_t *bmp)
   el->peer_asnumber = hdr->asn;
 
   return 0;
-}
-
-static void reset_generator(bgpstream_format_t *format)
-{
-  bgpstream_elem_clear(STATE->elem);
-  STATE->end_of_elems = 0;
-  STATE->peer_hdr_done = 0;
-  bgpstream_parsebgp_upd_state_reset(&STATE->upd_state);
 }
 
 /* -------------------- RECORD FILTERING -------------------- */
@@ -180,10 +179,6 @@ int bs_format_bmp_create(bgpstream_format_t *format,
     return -1;
   }
 
-  if ((STATE->elem = bgpstream_elem_create()) == NULL) {
-    return -1;
-  }
-
   STATE->decoder.msg_type = PARSEBGP_MSG_TYPE_BMP;
 
   opts = &STATE->decoder.parser_opts;
@@ -202,44 +197,45 @@ bgpstream_format_status_t
 bs_format_bmp_populate_record(bgpstream_format_t *format,
                               bgpstream_record_t *record)
 {
-  reset_generator(format);
-  return bgpstream_parsebgp_populate_record(&STATE->decoder, format, record,
-                                            populate_filter_cb);
+  return bgpstream_parsebgp_populate_record(&STATE->decoder, RDATA->msg, format,
+                                            record, populate_filter_cb);
 }
 
 int bs_format_bmp_get_next_elem(bgpstream_format_t *format,
                                 bgpstream_record_t *record,
                                 bgpstream_elem_t **elem)
 {
-  parsebgp_bmp_msg_t *bmp = BGPSTREAM_PARSEBGP_FDATA->types.bmp;
+  parsebgp_bmp_msg_t *bmp;
   int rc = 0;
   *elem = NULL;
 
-  if (BGPSTREAM_PARSEBGP_FDATA == NULL || STATE->end_of_elems != 0) {
+  if (RDATA == NULL || RDATA->end_of_elems != 0) {
     // end-of-elems
     return 0;
   }
 
+  bmp = RDATA->msg->types.bmp;
+
   // assume we'll find at least something juicy, so process the peer header and
   // fill the common parts of the elem
-  if (STATE->peer_hdr_done == 0 && handle_peer_hdr(STATE->elem, bmp) != 0) {
+  if (RDATA->peer_hdr_done == 0 && handle_peer_hdr(RDATA->elem, bmp) != 0) {
     return -1;
   }
-  STATE->peer_hdr_done = 1;
+  RDATA->peer_hdr_done = 1;
 
   // what kind of BMP message are we dealing with?
   switch (bmp->type) {
   case PARSEBGP_BMP_TYPE_ROUTE_MON:
     // TODO: explicitly handle end-of-RIB marker
-    rc = handle_update(format, bmp->types.route_mon);
+    rc = handle_update(RDATA, bmp->types.route_mon);
     break;
 
   case PARSEBGP_BMP_TYPE_PEER_DOWN:
-    rc = handle_peer_up_down(format, 0);
+    rc = handle_peer_up_down(RDATA, 0);
     break;
 
   case PARSEBGP_BMP_TYPE_PEER_UP:
-    rc = handle_peer_up_down(format, 1);
+    rc = handle_peer_up_down(RDATA, 1);
     break;
 
   default:
@@ -251,35 +247,57 @@ int bs_format_bmp_get_next_elem(bgpstream_format_t *format,
   }
 
   // return a borrowed pointer to the elem we populated
-  *elem = STATE->elem;
+  *elem = RDATA->elem;
   return 1;
 }
 
 int bs_format_bmp_init_data(bgpstream_format_t *format, void **data)
 {
-  if ((*data = parsebgp_create_msg()) == NULL) {
+  rec_data_t *rd;
+  *data = NULL;
+
+  if ((rd = malloc_zero(sizeof(rec_data_t))) == NULL) {
     return -1;
   }
+
+  if ((rd->elem = bgpstream_elem_create()) == NULL) {
+    return -1;
+  }
+
+  if ((rd->msg = parsebgp_create_msg()) == NULL) {
+    return -1;
+  }
+
+  *data = rd;
   return 0;
 }
 
 void bs_format_bmp_clear_data(bgpstream_format_t *format, void *data)
 {
-  reset_generator(format);
-  parsebgp_clear_msg((parsebgp_msg_t*)data);
+  rec_data_t *rd = (rec_data_t*)data;
+  assert(rd != NULL);
+  bgpstream_elem_clear(rd->elem);
+  rd->end_of_elems = 0;
+  rd->peer_hdr_done = 0;
+  bgpstream_parsebgp_upd_state_reset(&rd->upd_state);
+  parsebgp_clear_msg(rd->msg);
 }
 
 void bs_format_bmp_destroy_data(bgpstream_format_t *format, void *data)
 {
-  reset_generator(format);
-  parsebgp_destroy_msg((parsebgp_msg_t*)data);
+  rec_data_t *rd = (rec_data_t*)data;
+  if (rd == NULL) {
+    return;
+  }
+  bgpstream_elem_destroy(rd->elem);
+  rd->elem = NULL;
+  parsebgp_destroy_msg(rd->msg);
+  rd->msg = NULL;
+  free(data);
 }
 
 void bs_format_bmp_destroy(bgpstream_format_t *format)
 {
-  bgpstream_elem_destroy(STATE->elem);
-  STATE->elem = NULL;
-
   free(format->state);
   format->state = NULL;
 }
